56d51cb1e38fc2be8b99070e28f918c88bbbf4f4
tentaShiratori <tenta.shiratori@gmail.com>
2023-07-09 10:11:59 +0900

    fix

--- ses_sandbox/main.py
+++ ses_sandbox/main.py
@@ -1,24 +1,27 @@
-import boto3
 from mypy_boto3_ses import SESClient
 from mypy_boto3_sesv2 import SESV2Client
 from dotenv import load_dotenv
-import os
 from .ses_client import SesClient
 from os.path import join, dirname
 load_dotenv()
 
 
 def main():
-    # Create an ses client
-    ses:SESClient = boto3.client('ses', region_name='ap-northeast-1',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-    # Create an sesv2 client
-    sesv2:SESV2Client = boto3.client('sesv2', region_name='ap-northeast-1',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-    client = SesClient(sesv2)
-    client.send_email(
+    client = SesClient()
+    # client.v1_send_email(
+    #     from_="tenta.shiratori@gmail.com",
+    #     to="tenta.developer@gmail.com",
+    #     message="テストメールです",
+    #     attachments=[join(dirname(__file__), "./images/10MB.JPG")],
+    # )
+    client.v2_send_email(
         from_="tenta.shiratori@gmail.com",
         to="tenta.developer@gmail.com",
         message="テストメールです",
-        attachments=[join(dirname(__file__), "./images/10MB.JPG")],
+        attachments=[
+            join(dirname(__file__), "./images/10MB.JPG"),
+            join(dirname(__file__), "./images/10MB.JPG"),
+        ],
     )
 if __name__ == "__main__":
     main()
--- ses_sandbox/ses_client.py
+++ ses_sandbox/ses_client.py
@@ -3,23 +3,38 @@ from typing import List, Union
 from mypy_boto3_ses import SESClient
 from mypy_boto3_sesv2 import SESV2Client
 import imghdr
+import boto3
+import os
 
 
 class SesClient:
-    client: Union[SESClient, SESV2Client]
+    v1client: SESClient
+    v2client: SESV2Client
 
     def __init__(
-        self, client: Union[SESClient, SESV2Client]
+        self
     ):
-        self.client = client
+        self.v1client = boto3.client('ses', region_name='ap-northeast-1',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+        self.v2client = boto3.client('sesv2', region_name='ap-northeast-1',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+    
 
 
-    def send_email(self, from_:str, to:str,message:str, attachments:List[str]):
+    def v1_send_email(self, from_:str, to:str,message:str, attachments:List[str]):
         source = self._source(from_)
         msg = self._create_multipart_message(
             source, to, message, attachments
         )
-        self.client.send_email(
+        self.v1client.send_raw_email(
+            Source=source,
+            Destinations=[to],
+            RawMessage= {"Data": msg.as_string()},
+        )
+    def v2_send_email(self, from_:str, to:str,message:str, attachments:List[str]):
+        source = self._source(from_)
+        msg = self._create_multipart_message(
+            source, to, message, attachments
+        )
+        self.v2client.send_email(
             FromEmailAddress=source,
             Destination={"ToAddresses": [to]},
             Content={"Raw": {"Data": msg.as_string()}},
@@ -42,12 +57,14 @@ class SesClient:
 
         msg.set_content(message)
 
-        for attachment in attachments or []:
+        for (index, attachment) in enumerate(attachments):
+            _, file_extension = os.path.splitext(attachment)
             file= open(attachment, "rb")
             img_data = file.read()
             msg.add_attachment(
                 img_data,
                 maintype='image',
                 subtype=imghdr.what(None, img_data),
+                filename=f"image{index}{file_extension}",
             )
         return msg
